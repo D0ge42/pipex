94b2bb992bb7fa07d9b8d118d0bee7ad4171d102
Lorenzo Nulli <lonulli@e3r7p1.42roma.it>
2025-01-17 18:27:04 +0100

    started setting up pipex

--- pipex.c
+++ pipex.c
@@ -1,8 +1,29 @@
-#include <stdio.h>
+#include "pipex.h"
+#include "libft/libft.h"
+#include <cstring>
+#include <unistd.h>
 
-int	main(int ac, char **av)
+
+/*Check the existence of infile and outfile using F_OK*/
+
+int check_existence(const char *in, const char *out)
+{
+    if (access(in, F_OK) == 0 && access(out, F_OK) == 0)
+    {
+        ft_printf("File exists");
+        return 1;
+    }
+    return 0;
+}
+/*Function to return path of the given command*/
+char *pathfinder(char *cmd)
+{
+    char **path;
+    int i = 0;
+}
+
+int	main(int ac, char **av)//char **env)
 {
     (void)ac;
-    (void)av;
-	return (0);
+    check_existence(av[1],av[2]);
 }
