5194b4293c9df6c4d0a1bfc1c3b2836ba66ee705
doge421 <lorenzonulli42@gmail.com>
2025-01-18 18:49:37 +0100

    Made pathfinder function to retrieve path of binary exec and check its existence

--- pipex.c
+++ pipex.c
@@ -1,29 +1,58 @@
-#include "pipex.h"
 #include "libft/libft.h"
-#include <cstring>
+#include "pipex.h"
 #include <unistd.h>
 
+/*Function to check wheter the command exists or not
+We'll check for the word PATH= which means env variables are set.
+Once we find it, we can skip 5 chars and do split to get every path as word
+If access return 0, it means the current command is present in given path and we can return and free
+else we go to the next iteration*/
 
-/*Check the existence of infile and outfile using F_OK*/
+void free_paths(char **path)
+{
+    int i;
+    i = -1;
 
-int check_existence(const char *in, const char *out)
-{
-    if (access(in, F_OK) == 0 && access(out, F_OK) == 0)
-    {
-        ft_printf("File exists");
-        return 1;
-    }
-    return 0;
-}
-/*Function to return path of the given command*/
-char *pathfinder(char *cmd)
-{
-    char **path;
-    int i = 0;
+    while(path[++i] != NULL && path)
+        free(path[i]);
+    if(path)
+        free(path);
 }
 
-int	main(int ac, char **av)//char **env)
+char	*pathfinder(const char *cmd, char **env)
 {
-    (void)ac;
-    check_existence(av[1],av[2]);
+	char	**path;
+	int		i;
+	char	*return_path;
+	size_t	len;
+
+    i = 0;
+	while (!ft_strnstr((*env), "PATH=", 5))
+		env++;
+	path = ft_split(*env + 5, ':');
+	while (path[i] && path)
+	{
+		len = ft_strlen((char *)cmd) + ft_strlen(path[i]) + 2;
+		return_path = malloc(sizeof(char) * (len));
+		ft_memset(return_path, '\0', len);
+		ft_strlcat(return_path, path[i], len);
+		return_path[ft_strlen(path[i++])] = '/';
+		ft_strlcat(return_path, cmd, len);
+		if (access(return_path, F_OK) == 0)
+			break ;
+        free(return_path);
+	}
+    if (!path[i])
+    return_path = ft_strdup("");
+    free_paths(path);
+	return (return_path);
+}
+
+int	main(int ac, const char **av, char **env)
+{
+	(void)ac;
+	// check_existence(av[1],av[2]);
+	char *str = pathfinder(av[1], env);
+	ft_printf("%s\n", str);
+    free(str);
 }
